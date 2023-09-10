2dde57120f1625d6f1c9991f14ab922ec449e2c8
Vivek Pisal <54746512+vivekpisal@users.noreply.github.com>
2023-09-10 11:06:17 +0530

    Update SingletonRunner.java

--- Singleton/SingletonRunner.java
+++ Singleton/SingletonRunner.java
@@ -1,10 +1,11 @@
-package Creational.Singleton;
+// package Creational.Singleton;
 
 public class SingletonRunner {
     public static void main(String[] args){
-        EagerSingleton obj = EagerSingleton.getInstance();
-        EagerSingleton obj1 = EagerSingleton.getInstance();
-        System.out.println(obj.hashCode());
-        System.out.println(obj1.hashCode());
+        // EagerSingleton obj = EagerSingleton.getInstance();
+        // EagerSingleton obj1 = EagerSingleton.getInstance();
+        // System.out.println(obj.hashCode());
+        // System.out.println(obj1.hashCode());
+        System.out.println("Hello Jenkins from Github");
     }
 }
